4b788df4b55ff1664f7525e2308dee7d976a660c
Brad <BradPatras@users.noreply.github.com>
2026-08-10 22:26:46 -0500

    maze generation finished, that was surprisingly easy

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"math/rand/v2"
+	"slices"
 	"strings"
 
 	tea "charm.land/bubbletea/v2"
@@ -23,6 +25,7 @@ type vec2 struct {
 }
 
 type cell struct {
+	pos     vec2
 	walls   wallFlags
 	visited bool
 }
@@ -53,6 +56,8 @@ func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "ctrl+c", "q", "esc":
 			return m, tea.Quit
+		case "enter":
+			m.grid = generateMaze(((m.width)/4)-1, ((m.height)/2)-1)
 		}
 	case tea.WindowSizeMsg:
 		m.setWindowSize(msg.Width, msg.Height)
@@ -96,7 +101,8 @@ func createCellGrid(width int, height int) [][]cell {
 		grid[x] = make([]cell, height)
 		for y := range height {
 			grid[x][y] = cell{
-				walls: 15, // all walls, bitwise flags 1111
+				pos:   vec2{x, y},
+				walls: bitflags.Set(0, top, left, right, bottom),
 			}
 		}
 	}
@@ -114,27 +120,79 @@ func generateMaze(gridWidth int, gridHeight int) [][]cell {
 	grid := createCellGrid(gridWidth, gridHeight)
 
 	// entry point is top left
-	initial := &grid[0][0]
-	initial.walls = bitflags.Del(initial.walls, left)
-
+	current := &grid[0][0]
+	current.walls = bitflags.Del(current.walls, left)
+	current.visited = true
 	var backstack []vec2
-	backstack = append(backstack, vec2{0, 0})
+	backstack = append(backstack, current.pos)
 
 	// while backstack is not empty
 	for len(backstack) > 0 {
 		// pick a random un-visited neighbor of the last backstack element
-		
+		// collect unvisited neighbors
+		leftPos := vec2{current.pos.x - 1, current.pos.y}
+		rightPos := vec2{current.pos.x + 1, current.pos.y}
+		topPos := vec2{current.pos.x, current.pos.y - 1}
+		bottomPos := vec2{current.pos.x, current.pos.y + 1}
+		var neighbors []vec2
+
+		if leftPos.isInBounds(gridWidth, gridHeight) && !grid[leftPos.x][leftPos.y].visited {
+			neighbors = append(neighbors, leftPos)
+		}
+		if rightPos.isInBounds(gridWidth, gridHeight) && !grid[rightPos.x][rightPos.y].visited {
+			neighbors = append(neighbors, rightPos)
+		}
+		if topPos.isInBounds(gridWidth, gridHeight) && !grid[topPos.x][topPos.y].visited {
+			neighbors = append(neighbors, topPos)
+		}
+		if bottomPos.isInBounds(gridWidth, gridHeight) && !grid[bottomPos.x][bottomPos.y].visited {
+			neighbors = append(neighbors, bottomPos)
+		}
+
+		if len(neighbors) == 0 {
+			// no valid neighbors, move backwards
+			lastIndex := len(backstack) - 1
+			backstack = slices.Delete(backstack, lastIndex, lastIndex+1)
+			if len(backstack) < 1 {
+				// backstack is empty, no more cells to visit
+				break
+			}
+			previousPos := backstack[lastIndex-1]
+			current = &grid[previousPos.x][previousPos.y]
+			continue
+		}
+
 		// break down the wall between the backstack element and the neighbor
+		pickedNeighborPos := neighbors[rand.IntN(len(neighbors))]
+		pickedNeighbor := &grid[pickedNeighborPos.x][pickedNeighborPos.y]
+		if pickedNeighborPos.x < current.pos.x {
+			current.walls = bitflags.Del(current.walls, left)
+			pickedNeighbor.walls = bitflags.Del(pickedNeighbor.walls, right)
+		} else if pickedNeighborPos.x > current.pos.x {
+			current.walls = bitflags.Del(current.walls, right)
+			pickedNeighbor.walls = bitflags.Del(pickedNeighbor.walls, left)
+		} else if pickedNeighborPos.y < current.pos.y {
+			current.walls = bitflags.Del(current.walls, top)
+			pickedNeighbor.walls = bitflags.Del(pickedNeighbor.walls, bottom)
+		} else if pickedNeighborPos.y > current.pos.y {
+			current.walls = bitflags.Del(current.walls, bottom)
+			pickedNeighbor.walls = bitflags.Del(pickedNeighbor.walls, top)
+		}
 
 		// mark neighbor as visited and append it to the backstack
-
-		// if last backstack element has no un-visited neighbors, walk backwards
-		// through the backstack, removing cells until a cell with un-visited neighbors
-		// is found.
-
+		pickedNeighbor.visited = true
+		backstack = append(backstack, pickedNeighborPos)
+		current = pickedNeighbor
 	}
 
+	// create exit point in bottom right
+	grid[gridWidth-1][gridHeight-1].walls = bitflags.Del(grid[gridWidth-1][gridHeight-1].walls, right)
+
 	return grid
+}
+
+func (v vec2) isInBounds(width int, height int) bool {
+	return v.x > -1 && v.y > -1 && v.x < width && v.y < height
 }
 
 // per-cell render
